30326d13403b878a332e795a45e288d8d82d4700
Meeyoung Park <parkmee@gmail.com>
2023-03-07 23:27:57 -0500

    clean code

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # dependencies
 import pandas as pd
-import sqlalchemy
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from sqlalchemy import create_engine, func
 from config import protocol, username, password, port, database_name, host
 
-# print(sqlalchemy.__version__)
 ##################################################################################
 # connect to postgres database
 rds_connection_string = f'{protocol}://{username}:{password}@{host}:{port}/{database_name}'
@@ -114,42 +112,6 @@ def market_map(fips):
     with open(filepath, 'r', encoding='utf-8') as f:
         data = f.read()
     return data
-
-##################################################################################
-# route for info box
-# @app.route('/api/summary/<fips>')
-# def food_access_by_state(fips):
-#     # create DB session
-#     conn = engine.connect()
-    
-#     # import data from postgres
-#     query = 'select * from summary where "StateFIPS" = ' + "'" + fips + "'"
-#     # food_access_df = pd.read_sql_query(query, conn)
-#     food_access_df = pd.DataFrame(conn.execute(query))
-        
-#     print(food_access_df.tail())
-    
-#     # send json data
-#     food_access_json = food_access_df.to_json(orient='records', index=True)
-#     return food_access_json
-
-#     # retrieve data from postgres
-#     query = "select * from food_access where StateFIPS = '" + fips + "'"
-#     state_df = pd.read_sql(query, conn)
-    
-#     # derive state stats
-    
-#     dict = {
-#         # Population
-#         # Number of counties
-#         # Number of tracts
-#         # Number of urban tracts
-#         # Number of rural tracts
-#         # Number of urban tracts with flag
-#         # Number of rural tracts with flag
-#     }
-    
-#     return jsonify(dict)
 
 ##################################################################################
 # run app
